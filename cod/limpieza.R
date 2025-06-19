74261fff5d1a872bae47bef6a156000f438c4962
José Carlos Quintero <jose.quinterocedeno@ucr.ac.cr>
2025-06-19 11:32:49 -0600

    fix: arreglos en script de limpieza

--- cod/limpieza.R
+++ cod/limpieza.R
@@ -4,7 +4,7 @@ library(tidyverse)
 datos <- read_excel("data/DataREZUManonimos.xlsx", sheet = "Datos")
 #Se seleccionan las columnas mas importantes
 datos <- datos %>% select(
-  codigo,
+  id,
   edad,
   volumen.previo = volpre,
   volumen.posterior = volpost,
@@ -18,7 +18,10 @@ datos <- datos %>% select(
 #Se transforman las observaciones sin edad para que se tenga la edad promedio y el rao se codifica como un booleano
 datos <- datos %>%
   mutate(edad = if_else(is.na(edad), round(mean(edad, na.rm = TRUE)), edad)) %>%
-  mutate(rao = if_else(is.na(rao), 0, rao, ))
+  mutate(rao = if_else(is.na(rao), 0, rao, )) %>% 
+  mutate(delta.volumen = volumen.previo - volumen.posterior) %>% 
+  mutate(delta.ipss = ipss.previo - ipss.posterior) %>% 
+  mutate(delta.flujo = flujo.previo - flujo.posterior)
 
 #Se eliminan observaciones que no contribuyen información de volumen, ipss ni flujo
 datos <- datos %>%
@@ -30,6 +33,14 @@ datos <- datos %>%
       ipss.posterior,
       flujo.previo,
       flujo.posterior
+    ),
+    is.na
+  )) %>% 
+  filter(!if_all(
+    c(
+      delta.volumen,
+      delta.ipss,
+      delta.flujo
     ),
     is.na
   ))
